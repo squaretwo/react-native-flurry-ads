24446aace44f602170a556c082ecc3236e0e2ccb
MK Kang <mkkang0116@gmail.com>
2018-02-26 01:56:42 -0800

    Bug fix

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import { NativeEventEmitter, NativeModules } from 'react-native';
 import FlurryNativeAdsTrackingView from './FlurryNativeAdsTrackingView';
 
 class FlurryAds {
+  static fetchCallbacks = {};
   static clickEventCallbacks = {};
   static errorCallbacks = {};
 
   static initAd(adSpaceName, fetchedCallback, errorCallback) {
-    NativeModules.RNFlurryAds.initAd(adSpaceName, fetchedCallback);
+    NativeModules.RNFlurryAds.initAd(adSpaceName);
+    FlurryAds.fetchCallbacks[adSpaceName] = fetchedCallback;
     FlurryAds.errorCallbacks[adSpaceName] = errorCallback;
   }
 
@@ -22,6 +24,7 @@ class FlurryAds {
     NativeModules.RNFlurryAds.destroyAd(adSpaceName);
     FlurryAds.clickEventCallbacks[adSpaceName] = undefined;
     FlurryAds.errorCallbacks[adSpaceName] = undefined;
+    FlurryAds.fetchCallbacks[adSpaceName] = undefined;
   }
 }
 
@@ -30,19 +33,28 @@ const flurryAdEmitter = new NativeEventEmitter(NativeModules.RNFlurryAds);
 flurryAdEmitter.addListener(
   'EventClickAd',
   (adSpaceName) => {
-      if(FlurryAds.clickEventCallbacks[adSpaceName]) {
-        FlurryAds.clickEventCallbacks[adSpaceName]();
-      }
-  }
+  if(FlurryAds.clickEventCallbacks[adSpaceName]) {
+  FlurryAds.clickEventCallbacks[adSpaceName]();
+}
+}
 );
 
 flurryAdEmitter.addListener(
   'EventError',
   (params) => {
-    if(FlurryAds.errorCallbacks[params.adSpaceName]) {
-      FlurryAds.errorCallbacks[params.adSpaceName](params.errorType, params.errorCode);
-    }
-  }
+  if(FlurryAds.errorCallbacks[params.adSpaceName]) {
+  FlurryAds.errorCallbacks[params.adSpaceName](params.errorType, params.errorCode);
+}
+}
+);
+
+flurryAdEmitter.addListener(
+  'EventFetch',
+  (params) => {
+  if(FlurryAds.fetchCallbacks[params.adSpaceName]) {
+  FlurryAds.fetchCallbacks[params.adSpaceName](params.adData);
+}
+}
 );
 
 module.exports = { FlurryAds, FlurryNativeAdsTrackingView };
