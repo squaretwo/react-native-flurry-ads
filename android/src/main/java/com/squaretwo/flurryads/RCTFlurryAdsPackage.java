fd5255b16757282e4cc316b4451868aa75481ee7
Raymond Cook <lunarraid@gmail.com>
2018-05-18 11:01:05 -0400

    Add in deprecated override again

--- android/src/main/java/com/squaretwo/flurryads/RCTFlurryAdsPackage.java
+++ android/src/main/java/com/squaretwo/flurryads/RCTFlurryAdsPackage.java
@@ -18,6 +18,11 @@ import java.util.List;
 public class RCTFlurryAdsPackage implements ReactPackage {
 
     @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList(new RCTFlurryAdViewManager());
     }
